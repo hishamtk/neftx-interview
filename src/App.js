475f62b0b6a78ee9a5a94fc500a0b13aa8544a9b
hishamtk <tkhisham007@gmail.com>
2021-10-28 15:04:49 +0530

    added restaurants

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </div>
 
       <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 className="display-4">Find Restaurants</h1>
+        <h1 className="display-4">Find Stores</h1>
         <p className="lead">
         </p>
       </div> <div className="container px-4 py-5" id="custom-cards">
@@ -19,20 +19,21 @@ function App() {
 
     <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
 
-      {recipies.map(item =>(
+      {recipies.map(recipie =>(
         <div class="card mb-4 box-shadow mx-2">
         <div class="card-header">
-          <h4 class="my-0 font-weight-normal">{item.title}</h4>
+          <h4 class="my-0 font-weight-normal">{recipie.title}</h4>
         </div>
         <div class="card-body">
-          <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+          <h5 class="card-title pricing-card-title"> Course: {recipie.course}</h5>
+          <h6>Cuisine: {recipie.cuisine}</h6>
           <ul class="list-unstyled mt-3 mb-4">
-            <li>10 users included</li>
-            <li>2 GB of storage</li>
-            <li>Email support</li>
-            <li>Help center access</li>
+            {recipie.ingredients.map((ingradient,index)=>(
+              <li key={index} >{ingradient.name} : {ingradient.quantity}</li>
+            ))}
+          
           </ul>
-          <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          <button type="button" class="btn btn-lg btn-block btn-outline-primary">Find Store</button>
         </div>
       </div>
       ))}
